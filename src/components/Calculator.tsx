fb2962d781ad8f3b1e87ee88d26f7f372be75ab5
Nathaniel Johnson <njohnsonj@outlook.com>
2020-10-22 14:59:26 -0500

    Add space and validate after submitting
    Add space around the component.
    Validate the input after the user submits to allow for trailing operands while they are typing.

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -2,18 +2,19 @@ import React, { useState } from "react";
 import { FormControl, Card, CardContent, Grid, Input, Button, Typography, InputLabel } from "@material-ui/core"
 import { Database } from "../api/database";
 import firebase from "firebase";
+import styled from "styled-components";
 
 // From the material ui source
-type Color = 
-      | 'initial'
-      | 'inherit'
-      | 'primary'
-      | 'secondary'
-      | 'textPrimary'
-      | 'textSecondary'
-      | 'error';
+type Color =
+    | 'initial'
+    | 'inherit'
+    | 'primary'
+    | 'secondary'
+    | 'textPrimary'
+    | 'textSecondary'
+    | 'error';
 
-const Calculator: React.FC<{database: Database}> = ({database}) => {
+const Calculator: React.FC<{ database: Database }> = ({ database }) => {
 
     const [input, setInput] = useState<string>("");
     const [message, setMessage] = useState<string>("TRY TO CONFOUND ME, HUMAN.");
@@ -21,12 +22,8 @@ const Calculator: React.FC<{database: Database}> = ({database}) => {
 
     function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
         e.preventDefault();
-        if (validateInput(e.target.value)) {
-            setInput(e.target.value);
-            generateMessage();
-        } else {
-            generateErrorMessage();
-        }
+        setInput(e.target.value);
+        generateMessage();
     }
 
     function generateMessage() {
@@ -40,13 +37,19 @@ const Calculator: React.FC<{database: Database}> = ({database}) => {
         setColor("primary")
         setMessage("BEHOLD. MY TRANSISTORS ARE GREATER THAN YOUR NEURONS.")
     }
-    
+
     function generateErrorMessage() {
         setMessage("MATH DOES NOT CONTAIN LETTERS, HUMAN.")
         setColor("error");
     }
 
     function handleClick(e: React.MouseEvent<HTMLButtonElement>) {
+
+        if (!validateInput(input)) {
+            generateErrorMessage();
+            return;
+        }
+
         generateSuccessMessage();
 
         let result = "DOES NOT COMPUTE";
@@ -67,35 +70,41 @@ const Calculator: React.FC<{database: Database}> = ({database}) => {
     }
 
     function validateInput(value: string) {
-        return value.match(/^$|^\-{0,1}[0-9]+(?:[+\-\/*%]\-{0,1}[0-9]+)*$/);
+        return value.match(/^$|^\-{0,1}[0-9\s]+(?:[+\-\/*%]\-{0,1}[0-9\s]+)*$/);
     }
 
     return (
-        <Card>
-            <CardContent>
-                <Grid item>
-                    <Typography color={color}>{message}</Typography>
-                </Grid>
-                <FormControl>
-                    <InputLabel htmlFor="calculator-input">Formula</InputLabel>
-                    <Input
-                        id="calulator-input"
-                        type="input"
-                        value={input}
-                        onChange={handleChange}
-                    />
-                    <Button 
-                        type="submit" 
-                        onClick={handleClick} 
-                        color="primary" 
-                        variant="contained"
-                    >
-                        =
+        <Wrapper>
+            <Card>
+                <CardContent>
+                    <Grid item>
+                        <Typography color={color}>{message}</Typography>
+                    </Grid>
+                    <FormControl>
+                        <InputLabel htmlFor="calculator-input">Formula</InputLabel>
+                        <Input
+                            id="calulator-input"
+                            type="input"
+                            value={input}
+                            onChange={handleChange}
+                        />
+                        <Button
+                            type="submit"
+                            onClick={handleClick}
+                            color="primary"
+                            variant="contained"
+                        >
+                            =
                     </Button>
-                </FormControl>
-            </CardContent>
-        </Card>
+                    </FormControl>
+                </CardContent>
+            </Card>
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.div`
+    margin-top: 1em;
+`;
 
 export default Calculator;
