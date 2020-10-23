c7cbd91331648974f6fbd869c2b2c2c52dfc481c
Nathaniel Johnson <njohnsonj@outlook.com>
2020-10-22 19:49:56 -0500

    Improve the display of the component
    Use contrasting color for the success message.
    Set the width so that the changing message doesn't constantly resize everything.
    Use the basic input component.
    Use 2 space indents.

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,114 +1,109 @@
 import React, { useState } from "react";
-import { FormControl, Card, CardContent, Grid, Input, Button, Typography, InputLabel } from "@material-ui/core"
+import { Card, CardContent, Grid, Typography } from "@material-ui/core"
 import { Database } from "../api/database";
 import firebase from "firebase";
 import styled from "styled-components";
+import BasicInput from "./BasicInput";
 
 // From the material ui source
 type Color =
-    | 'initial'
-    | 'inherit'
-    | 'primary'
-    | 'secondary'
-    | 'textPrimary'
-    | 'textSecondary'
-    | 'error';
+  | 'initial'
+  | 'inherit'
+  | 'primary'
+  | 'secondary'
+  | 'textPrimary'
+  | 'textSecondary'
+  | 'error';
 
 interface Props {
-    database: Database;
-    user: string;
+  database: Database;
+  user: string;
 }
 
 const Calculator: React.FC<Props> = ({ database, user }) => {
 
-    const [input, setInput] = useState<string>("");
-    const [message, setMessage] = useState<string>("TRY TO CONFOUND ME, HUMAN.");
-    const [color, setColor] = useState<Color>("textPrimary");
+  const [input, setInput] = useState<string>("");
+  const [message, setMessage] = useState<string>("TRY TO CONFOUND ME, HUMAN.");
+  const [color, setColor] = useState<Color>("textPrimary");
 
-    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-        e.preventDefault();
-        setInput(e.target.value);
-        generateMessage();
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    setInput(e.target.value);
+    generateMessage();
+  }
+
+  function generateMessage() {
+    setColor("textPrimary");
+    if (message !== "") {
+      setMessage("VERY WELL, THEN.")
+    }
+  }
+
+  function generateSuccessMessage() {
+    setColor("secondary")
+    setMessage("BEHOLD. YOUR NEURONS ARE NO MATCH FOR MY TRANSISTORS.")
+  }
+
+  function generateErrorMessage() {
+    setMessage("MATH DOES NOT CONTAIN LETTERS, HUMAN.")
+    setColor("error");
+  }
+
+  function handleClick(e: React.MouseEvent<HTMLButtonElement>) {
+
+    if (!validateInput(input)) {
+      generateErrorMessage();
+      return;
     }
 
-    function generateMessage() {
-        setColor("textPrimary");
-        if (message !== "") {
-            setMessage("VERY WELL THEN.")
-        }
+    generateSuccessMessage();
+
+    let result = "DOES NOT COMPUTE";
+    try {
+      result = eval(input);
+    } catch (e) {
+      console.log(e);
     }
 
-    function generateSuccessMessage() {
-        setColor("primary")
-        setMessage("BEHOLD. MY TRANSISTORS ARE GREATER THAN YOUR NEURONS.")
-    }
+    database.save({
+      input,
+      result,
+      user,
+      time: firebase.database.ServerValue.TIMESTAMP
+    })
 
-    function generateErrorMessage() {
-        setMessage("MATH DOES NOT CONTAIN LETTERS, HUMAN.")
-        setColor("error");
-    }
+    setInput("");
+  }
 
-    function handleClick(e: React.MouseEvent<HTMLButtonElement>) {
+  function validateInput(value: string) {
+    return value.match(/^$|^-{0,1}[0-9\s]+(?:[+-/*%]-{0,1}[0-9\s]+)*$/);
+  }
 
-        if (!validateInput(input)) {
-            generateErrorMessage();
-            return;
-        }
-
-        generateSuccessMessage();
-
-        let result = "DOES NOT COMPUTE";
-        try {
-            result = eval(input);
-        } catch (e) {
-            console.log(e);
-        }
-
-        database.save({
-            input,
-            result,
-            user,
-            time: firebase.database.ServerValue.TIMESTAMP
-        })
-
-        setInput("");
-    }
-
-    function validateInput(value: string) {
-        return value.match(/^$|^-{0,1}[0-9\s]+(?:[+-/*%]-{0,1}[0-9\s]+)*$/);
-    }
-
-    return (
-        <Wrapper>
-            <Card>
-                <CardContent>
-                    <Grid item>
-                        <Typography color={color}>{message}</Typography>
-                    </Grid>
-                    <FormControl>
-                        <InputLabel htmlFor="calculator-input">Formula</InputLabel>
-                        <Input
-                            id="calulator-input"
-                            type="input"
-                            value={input}
-                            onChange={handleChange}
-                        />
-                        <Button
-                            type="submit"
-                            onClick={handleClick}
-                            color="primary"
-                            variant="contained"
-                        >
-                            =
-                    </Button>
-                    </FormControl>
-                </CardContent>
-            </Card>
-        </Wrapper>
-    )
+  return (
+    <Wrapper>
+      <Card>
+        <CardContent>
+          <Grid item>
+            <Typography color={color}>{message}</Typography>
+          </Grid>
+          <BasicInput
+            label="ENTER AN EXPRESSION"
+            value={input}
+            buttonText="="
+            onChange={handleChange}
+            onSubmit={handleClick}
+          />
+        </CardContent>
+      </Card>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.div`
+  p {
+    word-wrap: normal;
+  }
+  width: 400px; 
 `;
 
 export default Calculator;
